fd9a7a245fc662e666a86cbcd5373a5703288040
Swof <ivank1401@gmail.com>
2021-06-02 10:25:27 +1200

    finished mobile navigation

--- scripts/navbarDropDown.js
+++ scripts/navbarDropDown.js
@@ -1,14 +1,20 @@
 function drop() {
-    var x = document.getElementById("dropdownJS");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    var x = document.getElementById("nav");
+    if (x.classList.contains('mobile')) {
+        x.classList.remove('mobile');
     } else {
-        x.style.display = "block";
+        x.classList.add('mobile');
     }
     var y = document.getElementById("body");
     if (y.style.overflow === "hidden") {
         y.style.overflow = "unset";
     } else {
         y.style.overflow = "hidden";
+    }
+    var z = document.getElementById("icon");
+    if (z.classList.contains('open')) {
+        z.classList.remove('open');
+    } else {
+        z.classList.add('open');
     }
 }
